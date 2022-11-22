8d9d178a2182d9a2f29f79cad1a9ca450430f382
Cyril <cyrilondanje@gmail.com>
2022-11-23 00:06:33 +0300

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const DAY10Token = await hre.ethers.getContractFactory("DAY10Token")
+  const Day10Token =await DAY10Token.deploy(100000000, 50);
+
+  await Day10Token.deployed();
+
+  console.log("DAY10Token deployed: ",Day10Token.address)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
